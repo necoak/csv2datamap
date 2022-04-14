e37814aa880c6c7fb4471e5febcdb8cceccb06df
necoak <kaneko1983g@gmail.com>
2022-04-15 00:36:07 +0900

    とりあえず暫定でサンプルでのみ動く実装を作成

--- csv2datamap.py
+++ csv2datamap.py
@@ -1,5 +1,7 @@
+import codecs
 import csv
 from pprint import pprint
+
 
 class ColumnDefinitions:
     def __init__(self):
@@ -8,9 +10,15 @@ class ColumnDefinitions:
     def add(self, column_name):
         self.colum_definitions.append(ColumnDefinition(column_name))
 
+
 class ColumnDefinition:
     def __init__(self, column_name):
         self.column_name = column_name
+
+
+class HtmlPrinter:
+    pass
+
 
 def main():
     with open('sample/baseball2020ja.csv', encoding='utf_8_sig') as csvfile:
@@ -53,7 +61,118 @@ def main():
                 else:
                     print(', '.join([v[4] for v in targetvals]))
 
+        html_printer = HtmlPrinter()
+        html_printer.setup()
+        html_printer.start_body()
+        html_printer.start_table()
+        html_printer.start_table_header()
+        for position in positions:
+            html_printer.add_table_header_column(position)
+        html_printer.close_table_header()
+        html_printer.start_table_body()
+        for team in teams:
+            html_printer.start_table_record(team)
+            for position in positions:
+                target_players = []
+                for value in values:
+                    if (value[0] == team) and (value[1] == position):
+                        target_players.append(value[4])
+                html_printer.add_table_body_cell(target_players)
+            html_printer.close_table_record()
+        html_printer.close_table()
+        html_printer.close_body()
+        html_printer.setDown()
 
+        html_printer.print()
+
+
+
+class HtmlPrinter:
+    def __init__(self):
+        self.content = []
+
+    def setup(self):
+        self.add('''
+            <!DOCTYPE html>
+                <html lang="ja">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>出力例</title>
+                    <style type="text/css">
+                        table, td {
+                            border: solid;
+                        }
+                    table tr td:nth-of-type(1){
+                        background-color: #333;
+                        color: #fff;
+                    }
+                    thead, tfoot {
+                        background-color: #333;
+                        color: #fff;
+                    }
+                    div {
+                        background-color: #BAD3FF;
+                        margin : 10px;
+                        }
+                    </style>
+                </head>
+            ''')
+
+    def setDown(self):
+        self.add('''
+                </body>
+            </html>
+        ''')
+
+    def start_body(self):
+        self.add('<body>')
+
+    def close_body(self):
+        self.add('</body>')
+
+    def start_table(self):
+        self.add('<table>')
+
+    def close_table(self):
+        self.add('</table>')
+
+    def start_table_header(self):
+        self.add('''
+            <thead>
+                <tr>
+            ''')
+        self.add_table_header_column('#')
+
+    def add_table_header_column(self, text):
+        self.add('<th>%s</th>' % text)
+
+    def close_table_header(self):
+        self.add('''
+                </tr>
+            </thead>
+            ''')
+
+    def start_table_body(self):
+        self.add('<tbody>')
+
+    def start_table_record(self, text):
+        self.add('<tr>')
+        self.add('<td>%s</td>' % text)
+
+    def add_table_body_cell(self, values):
+        self.add('<td>')
+        for value in values:
+            self.add('<div>%s</div>' % value)
+        self.add('</td>')
+
+    def close_table_record(self):
+        self.add('</tr>')
+
+    def add(self, text):
+        self.content.append(text)
+
+    def print(self):
+        print('\n'.join(self.content), file=codecs.open('out.html', 'w', 'utf-8'))
 
 
 if __name__ == "__main__":
