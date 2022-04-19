d3c87389e830c6a073b46ee848111604322866d4
necoak <kaneko1983g@gmail.com>
2022-04-20 00:37:18 +0900

    refactor: 行列1要素での汎用化完了

--- csv2datamap.py
+++ csv2datamap.py
@@ -1,3 +1,4 @@
+import click
 import codecs
 import csv
 from pprint import pprint
@@ -20,156 +21,86 @@ class HtmlPrinter:
     pass
 
 
-def main():
-    with open('sample/baseball2020ja.csv', encoding='utf_8_sig') as csvfile:
+@click.command()
+@click.argument('filename', type=click.Path(exists=True))
+@click.argument('rowname', type=click.STRING)
+@click.argument('columnname', type=click.STRING)
+@click.argument('displayname', type=click.STRING)
+def main(filename, rowname, columnname, displayname):
+    with open(filename, encoding='utf_8_sig') as csvfile:
         csvreader = csv.reader(csvfile)
 
-        column_definitions = []
-        values = []
+        csv_column_definitions = []
+        csv_values = []
 
         for row in csvreader:
-            if len(column_definitions) == 0:
-                column_definitions = row
+            if len(csv_column_definitions) == 0:
+                csv_column_definitions = row
             else:
-                values.append(row)
+                csv_values.append(row)
 
-        print('球団マスタ')
-        teams = []
-        for value in values:
-            team = value[0]
-            if team not in teams:
-                teams.append(team)
-        pprint(teams)
+        csv_column_index_for_row = csv_column_definitions.index(rowname)
+        csv_column_index_for_column = csv_column_definitions.index(columnname)
+        csv_column_index_for_display = csv_column_definitions.index(displayname)
 
-        print('ポジションマスタ')
-        positions = []
-        for value in values:
-            position = value[1]
-            if position not in positions:
-                positions.append(position)
-        pprint(positions)
+        # Row Definition
+        row_definitions = []
+        for csv_value in csv_values:
+            tmp_row_definition = csv_value[csv_column_index_for_row]
+            if tmp_row_definition not in row_definitions:
+                row_definitions.append(tmp_row_definition)
+        pprint(row_definitions)
 
-        for team in teams:
-            for position in positions:
-                print('%s %s : ' % (team, position), end='')
+        # Column Definition
+        column_definitions = []
+        for csv_value in csv_values:
+            tmp_column_definition = csv_value[csv_column_index_for_column]
+            if tmp_column_definition not in column_definitions:
+                column_definitions.append(tmp_column_definition)
+        pprint(column_definitions)
+
+        # Row x Column で構造化
+        for row_definition in row_definitions:
+            for column_definition in column_definitions:
+                print('%s %s : ' % (row_definition, column_definition), end='')
                 targetvals = []
-                for value in values:
-                    if (value[0] == team) and (value[1] == position):
-                        targetvals.append(value)
+                for csv_value in csv_values:
+                    if (csv_value[csv_column_index_for_row] == row_definition) and \
+                            (csv_value[csv_column_index_for_column] == csv_column_index_for_column):
+                        targetvals.append(csv_value)
                 if len(targetvals) == 0:
                     print('None')
                 else:
                     print(', '.join([v[4] for v in targetvals]))
 
         html_printer = HtmlPrinter()
-        for position in positions:
-            html_printer.add_table_header_column(position)
-        for team in teams:
+        for column_definition in column_definitions:
+            html_printer.add_table_header_column(column_definition)
+        for row_definition in row_definitions:
             _record = []
-            _record.append(team)
-            for position in positions:
-                target_players = []
-                for value in values:
-                    if (value[0] == team) and (value[1] == position):
-                        target_players.append(value[4])
-                _record.append(target_players)
+            _record.append(row_definition)
+            for column_definition in column_definitions:
+                target_elements = []
+                for csv_value in csv_values:
+                    if ((csv_value[csv_column_index_for_row] == row_definition) and
+                            (csv_value[csv_column_index_for_column] == column_definition)):
+                        target_elements.append(csv_value[csv_column_index_for_display])
+                _record.append(target_elements)
             html_printer.add_table_record(_record)
 
         html_printer.print()
 
 
-
 class HtmlPrinter:
     def __init__(self):
-        self.content = []
         self._table_headers = []
         self._table_records = []
-
-    def setup(self):
-        self.add('''
-            <!DOCTYPE html>
-                <html lang="ja">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>出力例</title>
-                    <style type="text/css">
-                        table, td {
-                            border: solid;
-                        }
-                    table tr td:nth-of-type(1){
-                        background-color: #333;
-                        color: #fff;
-                    }
-                    thead, tfoot {
-                        background-color: #333;
-                        color: #fff;
-                    }
-                    div {
-                        background-color: #BAD3FF;
-                        margin : 10px;
-                        }
-                    </style>
-                </head>
-            ''')
-
-    def setDown(self):
-        self.add('''
-                </body>
-            </html>
-        ''')
-
-    def start_body(self):
-        self.add('<body>')
-
-    def close_body(self):
-        self.add('</body>')
-
-    def start_table(self):
-        self.add('<table>')
-
-    def close_table(self):
-        self.add('</table>')
-
-    def start_table_header(self):
-        self.add('''
-            <thead>
-                <tr>
-            ''')
-        self.add_table_header_column('#')
 
     def add_table_header_column(self, text):
         self._table_headers.append(text)
 
     def add_table_record(self, values):
         self._table_records.append(values)
-
-#    def add_table_header_column(self, text):
-#        self.add('<th>%s</th>' % text)
-
-    def close_table_header(self):
-        self.add('''
-                </tr>
-            </thead>
-            ''')
-
-    def start_table_body(self):
-        self.add('<tbody>')
-
-    def start_table_record(self, text):
-        self.add('<tr>')
-        self.add('<td>%s</td>' % text)
-
-    def add_table_body_cell(self, values):
-        self.add('<td>')
-        for value in values:
-            self.add('<div>%s</div>' % value)
-        self.add('</td>')
-
-    def close_table_record(self):
-        self.add('</tr>')
-
-    def add(self, text):
-        self.content.append(text)
 
     def print(self):
         content = []
