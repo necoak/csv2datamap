4b5129e9e510d3293d530333dacf519a447655f3
necoak <kaneko1983g@gmail.com>
2022-05-04 14:40:29 +0900

    不要コードの削除、デバッグプリント削除

--- csv2datamap.py
+++ csv2datamap.py
@@ -50,7 +50,6 @@ def generate_row1_col1_map(csv_values, template_html_text, csv_column_index_for_
 
 
 def is_match_kv2(key1, val1, key2, val2, kvlist):
-    print(key1, val1, key2, val2, kvlist)
     is_ok1, is_ok2 = False, False
     for k, v in kvlist:
         if (k == key1) and (v == val1):
@@ -76,35 +75,24 @@ def generate_rown_col1(csv_kvlist, template_html_text,
 
         row_definitions = convert_csv2kvlist(_csv_records)
 
-    pprint(row_definitions)
-    pprint('--------------------------')
-    pprint(csv_kvlist)
-
     mymap = []
     for row_definition in row_definitions:
         ret_record = [row_definition]
         key = None
-        pprint(row_definition)
         for k,v in row_definition:
             if k == rowname:
                 key = v
-        pprint(key)
 
         for column_key in csv_column_definition:
-            print('column_key ', column_key)
             ret_record.append(
                 list(filter(lambda kv: is_match_kv2(rowname, key, columnname, column_key, kv), csv_kvlist)))
         mymap.append(ret_record)
-    pprint('--------------------------')
-    pprint(mymap)
 
     html_printer = HtmlPrinter(template_html_text, len(row_definitions[0]))
     #
     for k,v in row_definitions[0]:
-        pprint(k)
         html_printer.add_table_header_column(k)
     for column_key in csv_column_definition:
-        pprint(column_key)
         html_printer.add_table_header_column(column_key)
     #
     for mymap_i in mymap:
@@ -118,13 +106,9 @@ def generate_rown_col1(csv_kvlist, template_html_text,
                     if ijk_k == displayname:
                         target_elements.append(ijk_v)
             _record.append(target_elements)
-        pprint('===========================')
-        pprint(_record)
         html_printer.add_table_record(_record)
 
     html_printer.print()
-
-
 
 
 def convert_csv2kvlist(csv_value):
@@ -184,56 +168,10 @@ def main(filename, rowname, columnname, displayname,
         if row_master is None:
             generate_row1_col1_map(csv_values, template_html_text, csv_column_index_for_row,
                                    csv_column_index_for_column, csv_column_index_for_display)
-            sys.exit(0)
-
-            for csv_value in csv_values:
-                tmp_row_definition = csv_value[csv_column_index_for_row]
-                if tmp_row_definition not in row_definitions:
-                    row_definitions.append(tmp_row_definition)
         else:
             generate_rown_col1(csv_kvlist, template_html_text,
                                rowname, columnname, displayname,
                                row_master, column_definitions)
-            sys.exit(9)
-            with open(row_master,   encoding='utf_8_sig') as row_master_csv_file:
-                row_master_csv_reader = csv.reader(row_master_csv_file)
-                for row_master in row_master_csv_reader:
-                    row_definitions.append(row_master[0])
-                row_definitions = row_definitions[1:]
-
-
-        # Column Definition
-        column_definitions = []
-        for csv_value in csv_values:
-            tmp_column_definition = csv_value[csv_column_index_for_column]
-            if tmp_column_definition not in column_definitions:
-                column_definitions.append(tmp_column_definition)
-
-        # Row x Column で構造化
-        for row_definition in row_definitions:
-            for column_definition in column_definitions:
-                targetvals = []
-                for csv_value in csv_values:
-                    if (csv_value[csv_column_index_for_row] == row_definition) and \
-                            (csv_value[csv_column_index_for_column] == csv_column_index_for_column):
-                        targetvals.append(csv_value)
-
-        html_printer = HtmlPrinter(template_html_text)
-        for column_definition in column_definitions:
-            html_printer.add_table_header_column(column_definition)
-        for row_definition in row_definitions:
-            _record = []
-            _record.append(row_definition)
-            for column_definition in column_definitions:
-                target_elements = []
-                for csv_value in csv_values:
-                    if ((csv_value[csv_column_index_for_row] == row_definition) and
-                            (csv_value[csv_column_index_for_column] == column_definition)):
-                        target_elements.append(csv_value[csv_column_index_for_display])
-                _record.append(target_elements)
-            html_printer.add_table_record(_record)
-
-        html_printer.print()
 
 
 class HtmlPrinter:
@@ -271,7 +209,6 @@ class HtmlPrinter:
             replace('<!---REPLACE1-->', '\n'.join(table_headers)).\
             replace('<!---REPLACE2-->', '\n'.join(table_bodies))
         print(html_text, file=codecs.open('./out/main.html', 'w', 'utf-8'))
-
 
 
 if __name__ == "__main__":
